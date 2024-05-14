5fbe27f6212dd6296ee7cf462023c6ba5979ab25
Ali Vidler <alisonvidler@gmail.com>
2024-05-14 14:23:33 -0400

    Making the spacing for the site

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,17 +8,17 @@ function Navbar({openModal}) {
 
   return (
     <div>
-      <nav>
-      <img src={santisticker} className="App-logo" alt="logo" />
-        <ul>
-          <li>
+      <nav className='flex flex-row gap-4 items-center'>
+      <img src={santisticker} className="App-logo object-scale-down h-20 p-2" alt="logo" />
+        <ul className='flex flex-row gap-2'>
+          <li className='p-2 rounded-lg bg-cyan-100'>
             <a href="/">Home</a>
           </li>
-          <li>
+          <li className='p-2 rounded-lg bg-cyan-100'>
             <a href="/photography">Photography</a>
           </li>
         </ul>
-        <button onClick={openModal}>+</button>
+        <button onClick={openModal} className='p-2 rounded-lg bg-cyan-100'>+</button>
       </nav>
     </div>
   )
