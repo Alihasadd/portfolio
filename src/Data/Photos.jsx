1274186d2a77c8519e07f9d452a61d1fad6e276c
Ali Vidler <alisonvidler@gmail.com>
2024-05-16 13:59:17 -0400

    Added photography

--- src/Data/Photos.jsx
+++ src/Data/Photos.jsx
@@ -3,6 +3,14 @@ import Counter from '../Assets/Counter.png'
 import couponApp from '../Assets/couponapp.png'
 import Sanfran from '../Assets/Sanfran.jpg'
 import Sandiego from '../Assets/Sandiego.jpg'
+import Navidad from '../Assets/Navidad.jpg'
+import Santicentral from '../Assets/Santicentral.jpg'
+import Santiwindow from '../Assets/Santiwindow.jpg'
+import Andes from '../Assets/Andes.jpg'
+import Santifocus from '../Assets/Santifocus.jpg'
+import Sunsetpark from '../Assets/Sunsetpark.jpg'
+import Santimiami from '../Assets/Santimiami.jpg'
+import Santinapa from '../Assets/Santinapa.jpg'
 
 const photos = [
     {
@@ -17,6 +25,62 @@ const photos = [
         title: 'San Diego',
         description: "Santiago walking along the water",
         image: Sandiego,
+        link: 'https://codepen.io/Alihasadd/pen/mdgpVrW',
+    },
+    {
+        id: 3,
+        title: 'Navidad',
+        description: "Navidad Chile",
+        image: Navidad,
+        link: 'https://codepen.io/Alihasadd/pen/mdgpVrW',
+    },
+    {
+        id: 4,
+        title: 'Central park',
+        description: "Santiago in Central Park",
+        image: Santicentral,
+        link: 'https://codepen.io/Alihasadd/pen/mdgpVrW',
+    },
+    {
+        id: 5,
+        title: 'Gramercy Park',
+        description: "Santiago at home",
+        image: Santiwindow,
+        link: 'https://codepen.io/Alihasadd/pen/mdgpVrW',
+    },
+    {
+        id: 6,
+        title: 'Andes Mountains',
+        description: "Flying over Chile",
+        image: Andes,
+        link: 'https://codepen.io/Alihasadd/pen/mdgpVrW',
+    },
+    {
+        id: 7,
+        title: 'Focused Santiago',
+        description: "Waiting for the ball",
+        image: Santifocus,
+        link: 'https://codepen.io/Alihasadd/pen/mdgpVrW',
+    },
+    {
+        id: 8,
+        title: 'Santiago Chile',
+        description: "Sunset Santiago Chile",
+        image: Sunsetpark,
+        link: 'https://codepen.io/Alihasadd/pen/mdgpVrW',
+    },
+    {
+        id: 9,
+        title: 'Miami Boating',
+        description: "Santi boating in Miami",
+        image: Santimiami,
+        link: 'https://codepen.io/Alihasadd/pen/mdgpVrW',
+    },
+    {
+        id: 10,
+        title: 'Santiago in Napa',
+        description: "Santi enjoying Napa's wine country",
+        image: Santinapa,
         link: 'https://codepen.io/Alihasadd/pen/mdgpVrW',
     },
    
