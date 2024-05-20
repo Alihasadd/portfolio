35b402138483528b490acb70c0f04713e89ff97d
Ali Vidler <alisonvidler@gmail.com>
2024-05-19 23:59:18 -0400

    Css change to grid

--- src/Components/CssGrid.jsx
+++ src/Components/CssGrid.jsx
@@ -15,7 +15,7 @@ const CssGrid = () => {
     <div className='grid grid-cols-3  dark:bg-rose-950 dark:text-white'>
         {data.map ((items, i) => (
             <div key={items.id} className='bg-neutral-100 border-2 rounded-lg m-3 w-50  dark:bg-rose-950 dark:text-white'>
-                <h2 className='font-extrabold text-2xl m-4 text-align-center  dark:bg-rose-950 dark:text-white'>{items.title}</h2>
+                <h2 className='font-extrabold text-2xl m-4 text-align-center  dark:bg-rose-950 dark:text-white dark:#613051'>{items.title}</h2>
                 <p className='m-4 text-align-justify  dark:bg-rose-950 dark:text-white'>{items.description}</p>
                 <p ><img src={items.image} alt="Code" className='p-1 border border-indigo-500/75 rounded-md' /></p>
             </div>
