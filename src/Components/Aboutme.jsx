b49d58ea4f9a3b64d331ea53ec3827d031e964a7
Ali Vidler <alisonvidler@gmail.com>
2024-05-29 12:26:56 -0700

    Added ul

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -10,6 +10,15 @@ function Aboutme() {
 
         <h3></h3>
       </div>
+
+      <ul className='bg-neutral-100 rounded-lg m-5 w-50 dark:bg-rose-950 dark:text-white group p-10'>
+          <li className='p-1'>2020 - How to jet ski </li>
+          <li className='p-1'>2019 - </li>
+          <li className='p-1'>2018 - Started learning French</li>
+          <li className='p-1'>2017 - Read 12 books</li>
+          <li className='p-1'>2016 - Front end web development</li>
+          <li className='p-1'>2015 - Front end web development</li>
+        </ul>
     </div>
   )
 }
