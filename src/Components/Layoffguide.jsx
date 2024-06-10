68d0d45427f89a333f9e1f793f139300d5bd3de7
Ali Vidler <alisonvidler@gmail.com>
2024-06-10 15:42:23 -0400

    Updated styling for the HR tab

--- src/Components/Layoffguide.jsx
+++ src/Components/Layoffguide.jsx
@@ -1,38 +1,32 @@
 import React from 'react'
 import loguide from '../Data/Loguide'
 import { useState } from 'react';
-
+import FAQ from './FAQ';
 
 function Layoffguide() {
 
     const [accordionOpen, setAccordionOpen] = useState(false);
     console.log(loguide)
   return (
-    <div className='p-4 bg-gray-100 rounded-lg border-pink-900 border-2 '>
+    <div className='p-4 bg-gray-100 rounded-lg border-pink-900 border-2 pt-20'>
       
 
       <button onClick={() => setAccordionOpen(!accordionOpen)} className='flex justify-between w-full'>
-            <span>{loguide.title}</span>
-            
-          
-            
-            
-            
-            {accordionOpen ? <span>-</span> : <span>+</span>}
+      
+ 
+  
             
       </button>
       
       {loguide.map ((item, i) => (
               <div>
-                <p>{item.id}</p>
-                <p>{item.title}</p>
-                <p>{item.answer}</p>
+                <FAQ title={item.title} answer={item.answer}/>
               </div>
             ))}
 
         <div className={`grid overflow-hidden transition-all duration-300 ease-in-out text-white text-sm ${accordionOpen ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}>
             <div className='overflow-hidden'>
-                {loguide.answer}
+    
             </div>
 
         </div>
