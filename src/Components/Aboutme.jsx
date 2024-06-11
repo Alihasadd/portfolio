08d62560aaa428f8baa60c5c0e2df77586c83e30
Ali Vidler <alisonvidler@gmail.com>
2024-06-10 23:46:15 -0400

    Added notes to the about me section

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -52,7 +52,7 @@ function Aboutme() {
 
       </div>
 
-      <div className='bg-neutral-100 rounded-lg m-10 w-50 dark:bg-blue-950 dark:text-white group hover:shadow-lg p-10'> 
+      <div className='bg-neutral-100 rounded-lg ml-10 w-50 dark:bg-blue-950 dark:text-white group hover:shadow-lg p-10'> 
         <h3 className='pb-2 justify-center text-lg font-extrabold'>Travel</h3>  
         <p>I love traveling and had the opportunity to do so to many countries</p>
         <ul className='bg-neutral-200 border border-slate-600 rounded-lg m-5 w-50 dark:bg-blue-950 dark:text-white group p-5 list-outside overflow-y-auto h-32 hover:shadow-inner'>
