35cf3cd5d17616fb18dee3bb6bb026c55ae69da8
Ali Vidler <alisonvidler@gmail.com>
2024-06-20 13:50:27 -0400

    Added more countries and still missing some

--- src/Data/Countries.js
+++ src/Data/Countries.js
@@ -1,6 +1,4 @@
-
 const countries = {
-
   PK: { year: "2021", scale: 60, phrase: "Ice cream walla" },
   US: { year: "Every year", scale: 100, phrase: "Yee haw!" },
   CA: { year: "2022, 2019, 2009", scale: 50, phrase: "Git statusssss" },
@@ -9,47 +7,19 @@ const countries = {
   CL: { year: "2023, 2019, 2016, ", scale: 90, phrase: "Weon" },
   MX: { year: "2017,2016", scale: 50, phrase: "Weyyy" },
   PR: { year: "2018, 2019,", scale: 50, phrase: "Sato" },
-  PR: { year: "2022, 2019, 2009", scale: 30, phrase: "Sato" },
-  // "AX": 60,
-  // "IN": 100,
-  // "CA": 10 ,
-  // "US": 100,
-  // "BE": 5,
-  // "AR": 10,
-  // "AM": "Armenia",
-  // "AW": "Aruba",
-  // "AU": "Australia",
-  // "AT": "Austria",
-  // "AZ": "Azerbaijan",
-  // "BS": "Bahamas",
-  // "BH": "Bahrain",
-  // "BD": "Bangladesh",
-  // "BB": "Barbados",
-  // "BY": "Belarus",
-
-  // "BZ": "Belize",
-  // "BJ": "Benin",
-  // "BM": "Bermuda",
-  // "BT": "Bhutan",
-  // "BO": "Bolivia",
-  // "BA": "Bosnia and Herzegovina",
-  // "BW": "Botswana",
-  // "BV": "Bouvet Island",
-  // "BR": "Brazil",
-  // "IO": "British Indian Ocean Territory",
-  // "BN": "Brunei Darussalam",
-  // "BG": "Bulgaria",
-  // "BF": "Burkina Faso",
-  // "BI": "Burundi",
-  // "KH": "Cambodia",
-  // "CM": "Cameroon",
-  // "CA": "Canada",
-  // "CV": "Cape Verde",
-  // "KY": "Cayman Islands",
-  // "CF": "Central African Republic",
-  // "TD": "Chad",
-  // "CL": "Chile",
-  // "CN": "China",
+  EC: { year: "2022", scale: 30, phrase: "" },
+  PE: { year: "2024", scale: 30, phrase: "Plop" },
+  CO: { year: "2018", scale: 30, phrase: "" },
+  CR: { year: "2022", scale: 30, phrase: "Pura Video" },
+  PA: { year: "2017, 2009", scale: 50, phrase: "" },
+  UY: { year: "2023, 2020", scale: 50, phrase: "" }, //Not found
+  PY: { year: "2023", scale: 30, phrase: "" },
+  EG: { year: "2021", scale: 30, phrase: "Mumtaz" },
+  AR: { year: "2023, 2019", scale: 30, phrase: "Mumtaz" },
+  GB: { year: "Several", scale: 100, phrase: "Init" },
+  GL: { year: "2022", scale: 30, phrase: "" },
+  IS: { year: "2016", scale: 30, phrase: "" },
+  ES: { year: "2011", scale: 30, phrase: "" },
   // "CX": "Christmas Island",
   // "CC": "Cocos (Keeling) Islands",
   // "CO": "Colombia",
@@ -77,16 +47,16 @@ const countries = {
 };
 
 export const countryYears = Object.keys(countries).reduce((acc, key) => {
-    acc[key] = countries[key].year;
-    return acc;
+  acc[key] = countries[key].year;
+  return acc;
 }, {});
 
 export const countryScales = Object.keys(countries).reduce((acc, key) => {
-    acc[key] = countries[key].scale;
-    return acc;
+  acc[key] = countries[key].scale;
+  return acc;
 }, {});
 
 export const countryPhrase = Object.keys(countries).reduce((acc, key) => {
-    acc[key] = countries[key].phrase;
-    return acc;
+  acc[key] = countries[key].phrase;
+  return acc;
 }, {});
