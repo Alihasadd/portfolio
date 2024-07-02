a348f2b4db1292d41ac9cf2fce2a98df01e4a96e
debasishrowlo <debasishrowlo@gmail.com>
2024-07-02 23:28:09 +0530

    Add containers, fix hero section

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,47 +12,43 @@ function Navbar({openModal, toggleDarkMode, darkMode, setIsModalOpen}) {
 
   return (
     <div className="fixed w-full z-10">
-        
-      <nav className='flex justify-between items-center bg-pink-700 dark:bg-blue-950 '>
-      <div className='flex items-center gap-1 '>
-        
-        
-       
-        <ul className='flex flex-row gap-1 '>
-          <li className='p-1 ml-2 rounded-lg w-16 flex justify-center hover:shadow-md hover:bg-pink-800 dark:md:hover:bg-blue-800 hover:dark:bg-blue-950 dark:bg-blue dark text-slate-200 dark:text-white'>
-            <Link to="/">Me</Link>
-          </li>
-          <li className='p-1 rounded-lg w-16 flex justify-center hover:shadow-md hover:bg-pink-800 dark:md:hover:bg-blue-800 hover:dark:bg-blue-950 dark:bg-blue dark text-slate-200 dark:text-white'>
-            <Link to="/code">Code</Link>
-          </li>
-          <li className='p-1 rounded-lg w-16 flex justify-center hover:shadow-md hover:bg-pink-800 dark:md:hover:bg-blue-800 hover:dark:bg-blue-950 dark:bg-purple dark text-slate-200 dark:text-white'>
-            <Link to="/photography">Photo</Link>
-          </li>
-          <li className='p-1 rounded-lg flex justify-center w-16 hover:shadow-md hover:bg-pink-800 dark:md:hover:bg-blue-800 hover:dark:bg-blue-950 dark:bg-purple dark text-slate-200 dark:text-white'>
-            <Link to="/accordion">FAQs</Link>
-          </li>
-          <li className='p-1 rounded-lg w-16 flex justify-center hover:shadow-md hover:bg-pink-800 dark:md:hover:bg-blue-800 hover:dark:bg-blue-950 dark:bg-purple dark text-slate-200 dark:text-white'>
-            <Link to="/hr">HR</Link>
-          </li>
-       </ul>
-        
-        </div>
-        <div className='flex items-center gap-1 mr-3'>
+      <div className="bg-pink-700 dark:bg-blue-950">
+        <nav className='container mx-auto flex justify-between items-center'>
+          <div className='flex items-center gap-1'>
+            <ul className='flex flex-row gap-1 '>
+              <li className='p-1 ml-2 rounded-lg w-16 flex justify-center hover:shadow-md hover:bg-pink-800 dark:md:hover:bg-blue-800 hover:dark:bg-blue-950 dark:bg-blue dark text-slate-200 dark:text-white'>
+                <Link to="/">Me</Link>
+              </li>
+              <li className='p-1 rounded-lg w-16 flex justify-center hover:shadow-md hover:bg-pink-800 dark:md:hover:bg-blue-800 hover:dark:bg-blue-950 dark:bg-blue dark text-slate-200 dark:text-white'>
+                <Link to="/code">Code</Link>
+              </li>
+              <li className='p-1 rounded-lg w-16 flex justify-center hover:shadow-md hover:bg-pink-800 dark:md:hover:bg-blue-800 hover:dark:bg-blue-950 dark:bg-purple dark text-slate-200 dark:text-white'>
+                <Link to="/photography">Photo</Link>
+              </li>
+              <li className='p-1 rounded-lg flex justify-center w-16 hover:shadow-md hover:bg-pink-800 dark:md:hover:bg-blue-800 hover:dark:bg-blue-950 dark:bg-purple dark text-slate-200 dark:text-white'>
+                <Link to="/accordion">FAQs</Link>
+              </li>
+              <li className='p-1 rounded-lg w-16 flex justify-center hover:shadow-md hover:bg-pink-800 dark:md:hover:bg-blue-800 hover:dark:bg-blue-950 dark:bg-purple dark text-slate-200 dark:text-white'>
+                <Link to="/hr">HR</Link>
+              </li>
+            </ul>
+          </div>
+          <div className='flex items-center gap-1 mr-3'>
             <button className=' w-10 h-10 rounded-full p-1 dark:bg-blue-950 text-white hover:shadow-md' onClick={toggleDarkMode}>{darkMode ?  <img src={lighticon}/> : <img src={darkicon}/>}</button>
-        <ul className='flex flex-row gap-1 items-center'>
-          
-          <li className='p-2 m-1 hover:shadow-md text-white dark:bg-blue-950 dark rounded-full '>
-            <a href="https://github.com/Alihasadd" target="_blank" rel="noopener noreferrer"><img src={githublogo} alt="GitHub Logo" className='h-6 rounded-full' /></a>
-          </li>
+            <ul className='flex flex-row gap-1 items-center'>
+              <li className='p-2 m-1 hover:shadow-md text-white dark:bg-blue-950 dark rounded-full '>
+                <a href="https://github.com/Alihasadd" target="_blank" rel="noopener noreferrer"><img src={githublogo} alt="GitHub Logo" className='h-6 rounded-full' /></a>
+              </li>
 
-          <li className='flex justify-center items-center hover:shadow-md text-white dark:bg-blue-950 dark rounded-full w-10 h-10 p-2'>
-            
-            <button  onClick={() => setIsModalOpen(true)} alt="GitHub Logo"><img src={santisticker1} className=' rounded-full  hover:drop-shadow-lg '/></button>
-            
-          </li>
-        </ul>
-        </div>
-      </nav>
+              <li className='flex justify-center items-center hover:shadow-md text-white dark:bg-blue-950 dark rounded-full w-10 h-10 p-2'>
+                
+                <button  onClick={() => setIsModalOpen(true)} alt="GitHub Logo"><img src={santisticker1} className=' rounded-full  hover:drop-shadow-lg '/></button>
+                
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
     </div>
   )
 }
