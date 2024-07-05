5ae2460c8fdf3fcc43a132ba81668f2b1ced2615
debasishrowlo <debasishrowlo@gmail.com>
2024-07-05 21:14:59 +0530

    Convert lists to arrays

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -24,11 +24,15 @@ function Aboutme() {
         <div className='bg-neutral-100 rounded-lg mr-10 ml-10 w-50 dark:bg-blue-950 dark:text-white group hover:shadow-lg p-10 lg:mx-0'>
           <h2 className='pb-2 justify-center text-lg font-extrabold'>My Skills</h2>
           <div className='divide-y-4 divide-black divide-opacity-25'>
-            <p className='py-4 '>I started my career as a Recruiter at JP Morgan Chase and then at CIBC (Canadian Imperial Bank of Commerce). While working at CIBC, I wanted a new challenge. Instead of working on legacy processes, I wanted to build out a scalable function, so I started looking to work at a startup. </p>
-            <p className='py-4'>I joined an Adtech company in New York City where I helped build out their recruiting and people operations practices including setting up their HR technology and implementing a new applicant tracking system.</p>
-            <p className='py-4'>While at a startup, I started to work with a tech team who taught me more about software engineering. I did a Front-end web development course at General Assembly course and I loved it. I wanted to keep learning and over the years I would gather more bits and pieces.</p>
-            <p className='py-4'>I wanted to be more immersed in tech, so I started looking for tech roles. I moved Talent tools, HR systems, and doing system implementations and the company's intranet and diving into content strategy on the employee experience engineering team.</p>
-            <p className='py-4'>It was there I got to get into the code base and do some software engineering work for the company. Making changes to our code base, pairing with other engineers, code review, collaborating with design, and getting more immersed in software engineering.</p>
+            {[
+              "I started my career as a Recruiter at JP Morgan Chase and then at CIBC (Canadian Imperial Bank of Commerce). While working at CIBC, I wanted a new challenge. Instead of working on legacy processes, I wanted to build out a scalable function, so I started looking to work at a startup. ",
+              "I joined an Adtech company in New York City where I helped build out their recruiting and people operations practices including setting up their HR technology and implementing a new applicant tracking system.",
+              "While at a startup, I started to work with a tech team who taught me more about software engineering. I did a Front-end web development course at General Assembly course and I loved it. I wanted to keep learning and over the years I would gather more bits and pieces.",
+              "I wanted to be more immersed in tech, so I started looking for tech roles. I moved Talent tools, HR systems, and doing system implementations and the company's intranet and diving into content strategy on the employee experience engineering team.",
+              "It was there I got to get into the code base and do some software engineering work for the company. Making changes to our code base, pairing with other engineers, code review, collaborating with design, and getting more immersed in software engineering.",
+            ].map((item, index) => (
+              <p className='py-4 last:pb-0' key={index}>{item}</p>
+            ))}
           </div>
         </div>
       
@@ -36,22 +40,26 @@ function Aboutme() {
           <h3 className='pb-2 justify-center text-lg font-extrabold'>We are all life long learners</h3>  
           <p>I found research that says the more you learn the more elastic your brain will be. I've made it a goal to always be looking for ways to learn, even if it's a small amount about a topic. Here are some of the things I've learned over the years:</p>
           <ul className='bg-neutral-200 border border-slate-600 rounded-lg m-5 w-50 dark:bg-zinc-700 dark:text-white group p-5 list-outside overflow-y-auto h-32 hover:shadow-inner'>
-            <li className='p-1'>2024 - Software Engineering</li>
-            <li className='p-1'>2023 - How to drive boats </li>
-            <li className='p-1'>2022 - That you can go into the pyramids in Egypt</li>
-            <li className='p-1'>2021 - Some simple Urdu phrases</li>
-            <li className='p-1'>2020 - How to jet ski</li>
-            <li className='p-1'>2019 - How to train dogs</li>
-            <li className='p-1'>2018 - Started learning French</li>
-            <li className='p-1'>2017 - Read 12 books</li>
-            <li className='p-1'>2016 - Some front end web development</li>
-            <li className='p-1'>2015 - How to get laid off</li>
-            <li className='p-1'>2014 - How People operations works</li>
-            <li className='p-1'>2013 - How to kayak</li>
-            <li className='p-1'>2012 - How the Talent acquisition process works</li>
-            <li className='p-1'>2011 - How Logistics works</li>
-            <li className='p-1'>2010 - How to navigate the NYC Subway</li>
-            <li className='p-1'>2009 - How to travel in Panama and Costa Rica</li>
+            {[
+              "2024 - Software Engineering",
+              "2023 - How to drive boats ",
+              "2022 - That you can go into the pyramids in Egypt",
+              "2021 - Some simple Urdu phrases",
+              "2020 - How to jet ski",
+              "2019 - How to train dogs",
+              "2018 - Started learning French",
+              "2017 - Read 12 books",
+              "2016 - Some front end web development",
+              "2015 - How to get laid off",
+              "2014 - How People operations works",
+              "2013 - How to kayak",
+              "2012 - How the Talent acquisition process works",
+              "2011 - How Logistics works",
+              "2010 - How to navigate the NYC Subway",
+              "2009 - How to travel in Panama and Costa Rica",
+            ].map((item, index) => (
+              <li className='p-1' key={index}>{item}</li>
+            ))}
           </ul>
         </div>
 
@@ -84,29 +92,21 @@ function Aboutme() {
           </ul>
         </div>
 
-        <div className='flex justify-between items-center overflow-hidden rounded-lg bg-neutral-100 m-10 dark:bg-blue-950 hover:shadow-lg lg:mx-0'>
-          <span className='p-2 text-lg font-lg object-right'>
-              <h1 className='object-right m-5 w-full md:w-1/2'>I like to combine my love of learning with travel. Here I am using my boating license while traveling in Miami</h1>
-          </span>
-
-        
-          
-
-
-          <main className="md:w-[700px] w-[60vw] overflow-hidden rounded-r-lg object-left">
-              <div id="slide1" className='rounded-r-lg object-left slide '>
-                
-                  <img className='rounded-r-lg object-left image' src={santimiami}/>
-
-
- {/* <a href="#slide3" className="prev arrow">
+        <div className='flex flex-col-reverse items-center justify-between overflow-hidden rounded-lg bg-neutral-100 m-10 dark:bg-blue-950 hover:shadow-lg md:flex-row lg:mx-0'>
+          <div className='p-5 text-lg font-lg object-right'>
+            <h1 className='object-right w-full md:w-1/2'>I like to combine my love of learning with travel. Here I am using my boating license while traveling in Miami</h1>
+          </div>
+          <div className="overflow-hidden md:w-[700px] md:self-stretch">
+            <div id="slide1" className='slide h-full'>
+              <img className='image object-cover' src={santimiami} />
+              {/* <a href="#slide3" className="prev arrow">
                       &lt;
                   </a>
                   <a href="#slide2" className="next arrow">
                       &gt;
                   </a>  */}
-              </div>
-              {/* <div id="slide2" className='rounded-r-lg slide'>
+            </div>
+            {/* <div id="slide2" className='rounded-r-lg slide'>
                   <img  className='rounded-r-lg image' src={alisanti}/>
                   <a href="#slide1" className="prev arrow">
                       &lt;
@@ -124,7 +124,7 @@ function Aboutme() {
                       &gt;
                   </a>
               </div> */}
-          </main>
+          </div>
         </div>
       </div>
     </div>
